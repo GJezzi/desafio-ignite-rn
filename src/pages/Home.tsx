773edb66af72d47214f7625322ab1eb6f17e3d67
Gianfranco Jezzi <gianfranco.jezzi@somosphi.com>
2021-05-28 19:24:02 -0300

    Deafio Resolvido

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { Header } from '../components/Header';
-import { MyTasksList } from '../components/MyTasksList';
-import { TodoInput } from '../components/TodoInput';
+import { Header } from "../components/Header";
+import { MyTasksList } from "../components/MyTasksList";
+import { TodoInput } from "../components/TodoInput";
 
 interface Task {
   id: number;
@@ -11,18 +11,35 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task if it's not empty
+    if (newTaskTitle !== "") {
+      const data = {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        done: false,
+      };
+      setTasks((oldTask) => [...oldTask, data]);
+      console.log(data);
+      return;
+    }
   }
 
   function handleMarkTaskAsDone(id: number) {
     //TODO - mark task as done if exists
+    const task = tasks.filter((item) => item.id === id)[0];
+
+    task.done = !task.done;
+
+    const doneTasks = [...new Set([task, ...tasks])];
+    setTasks(doneTasks);
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    setTasks((oldTask) => oldTask.filter((task) => task.id !== id));
   }
 
   return (
@@ -31,11 +48,11 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <MyTasksList 
-        tasks={tasks} 
-        onPress={handleMarkTaskAsDone} 
-        onLongPress={handleRemoveTask} 
+      <MyTasksList
+        tasks={tasks}
+        onPress={handleMarkTaskAsDone}
+        onLongPress={handleRemoveTask}
       />
     </>
-  )
+  );
 }
